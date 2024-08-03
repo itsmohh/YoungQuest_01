773de53f79b749d22ad3038fcab76da234c7ed29
Muhammed <olayinkafacus@gmail.com>
2024-08-03 14:30:23 -0400

    Initial Commit

---
+++ TeamBlue/TeamBlueApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamBlueApp.swift
+//  TeamBlue
+//
+//  Created by Yinka  Facus  on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TeamBlueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
