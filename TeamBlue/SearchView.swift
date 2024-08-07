870583d7f80149ed8864aaf08d1511351f867cb0
Muhammed <olayinkafacus@gmail.com>
2024-08-07 10:55:37 -0400

    Testing and Recovering Branches changes

---
+++ TeamBlue/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  TeamBlue
+//
+//  Created by Yinka  Facus  on 8/7/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SearchView()
+}
